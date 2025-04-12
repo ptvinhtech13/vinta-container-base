30ce450ab129bdde5a666ebcfe7e97243cc648bc
Vinh Pham <pham.the.vinh@styl.solutions>
2025-04-12 21:13:43 +0700

    init project

---
+++ src/main/java/io/vinta/transit/cms/TransitLlcCmsApplication.java
@@ -0,0 +1,13 @@
+package io.vinta.transit.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitLlcCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitLlcCmsApplication.class, args);
+	}
+
+}
